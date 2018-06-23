39cfbaa397120de962c0723198861e8169ad91c7
Philip Putnam <putnamp989@gmail.com>
2018-06-23 10:26:11 -0700

    importing classes, user input, and exception handling

--- src/main/java/FavoriteColor.java
+++ src/main/java/FavoriteColor.java
@@ -1,5 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class FavoriteColor {
     public static void main(String[] args) {
         System.out.println("What is your favorite color?");
+
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String inputString = bufferedReader.readLine();
+
+            System.out.println("color entered : " + inputString);
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
